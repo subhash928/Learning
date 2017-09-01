05f732fa005e14fa9ab3a6572b7a0a7ca1949a6d
Subhash Chandra Pasumarthi <subhash928@gmail.com>
2017-09-01 20:28:30 +0530

    Made Movements for Snake.

--- Scripts/Slither.py
+++ Scripts/Slither.py
@@ -4,6 +4,7 @@ pygame.init()
 
 black = (0,0,0)
 white = (255,255,255)
+maroon = (128,0,0)
 
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('Slither')
@@ -14,6 +15,14 @@ gameExit = False
 
 lead_x=300
 lead_y=300
+lead_x_change = 0
+lead_y_change = 0
+apple_x = 100
+apple_y = 100
+
+clock = pygame.time.Clock()
+
+
 
 while not gameExit:
     for event in pygame.event.get():
@@ -22,19 +31,39 @@ while not gameExit:
             gameExit = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                lead_x -= 10
+                lead_x_change = -10
+                lead_y_change = 0 #REM
             if event.key == pygame.K_RIGHT:
-                lead_x += 10
+                lead_x_change = 10
+                lead_y_change = 0 #REM
             if event.key == pygame.K_UP:
-                lead_y -= 10
+                lead_y_change = -10
+                lead_x_change = 0 #REM
             if event.key == pygame.K_DOWN:
-                lead_y += 10
-
-
+                lead_y_change = 10
+                lead_x_change = 0 #REM
+            if event.key == pygame.K_ESCAPE: # THIS MAKES THE GAME CLOSE WITH ESCAPE. :)
+                gameExit = True
+# FOR MAKING THE SNAKE GO TILL U RELEASE THE KEY REMOVE THE QUOTATIONS BELOW. AND REMOVE THE *ENTIRE LINES* WITH #REM ABOVE. :)
+        """if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                lead_x_change = 0
+            if event.key == pygame.K_RIGHT:
+                lead_x_change = 0
+            if event.key == pygame.K_UP:
+                lead_y_change = 0
+            if event.key == pygame.K_DOWN:
+                lead_y_change = 0"""
+    lead_x += lead_x_change
+    lead_y += lead_y_change
     gameDisplay.fill(white)
+    pygame.draw.rect(gameDisplay, maroon, [apple_x,apple_y,10,10])
     pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,10,10])
     pygame.display.update()
-
+    if lead_x == apple_x and lead_y == apple_y:
+        pygame.draw.rect(gameDisplay , white, [apple_x,apple_y,10,10])
+    pygame.display.update()
+    clock.tick(15)
     
 
 
